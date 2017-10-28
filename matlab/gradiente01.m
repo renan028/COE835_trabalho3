3f4ca268a777887f8656fe563710c461288730c2
renan028 <renan028@gmail.com>
2017-10-28 19:34:54 -0200

    ADD sim01 gradiente ok

--- matlab/gradiente01.m
+++ matlab/gradiente01.m
@@ -4,45 +4,42 @@
 %
 %  Script para simular o exemplo
 %
-%  Gradiente  : n  = 3     First order plant
+%  Gradiente  : n  = 1     First order plant
 %               n* = 1     Relative degree
-%               np = 6     Adaptive parameters
+%               np = 2     Adaptive parameters
 %
 %                                                        Ramon R. Costa
 %                                                        30/abr/13, Rio
 %======================================================================
-function dx=gradiente(t,x)
+function dx=gradiente01(t,x)
 
-global filter_param dc a w gamma am km thetas;
+global filter_param dc a w gamma thetas;
 
-ym     = x(1);
-theta = x(2:7);
-uf = x(8:10);
-yf = x(11:13);
+theta = x(1:2);
+uf = x(3);
+yf = x(4);
 
 %--------------------------
-r = dc + a*sin(w*t) + a*sin(2*w*t) + a*sin(3*w*t) + a*sin(4*w*t);
+r = dc + a*sin(w*t) + a*sin(2*w*t);
 
 phi = [uf' yf']';
 y = thetas'*phi;
-omega = [ y y y r r r]';
+omega = [ y r ]';
 
 u = theta.'*omega;
-duf = [uf(2:3)' u-(flip(filter_param)'*uf)]';
-dyf = [yf(2:3)' y-(flip(filter_param)'*yf)]';
+duf = [u-(flip(filter_param)'*uf)]';
+dyf = [y-(flip(filter_param)'*yf)]';
 
 
 yhat = theta'*phi;
 
 epsilon = yhat - y;
 
-dym = -am*ym + km*r;
-
 m2 = 1 + phi'*phi;
 
 dtheta = -gamma*phi*epsilon/m2;
 
 %--------------------------
-dx = [dym dtheta' duf' dyf']';    %Translation
+dx = [dtheta' duf' dyf']';    %Translation
 
 %---------------------------
--- matlab/simu03.m
+++ matlab/simu03.m
@@ -4,9 +4,9 @@
 %
 %  Script para simular exemplo 
 %
-%  MRAC  : n  = 1     First order plant
+%  MRAC  : n  = 3     First order plant
 %          n* = 1     Relative degree
-%          np = 2     Adaptive parameters
+%          np = 6     Adaptive parameters
 %
 %                                                        Ramon R. Costa
 %                                                        30/abr/13, Rio
@@ -17,75 +17,79 @@ clc;
 disp('-------------------------------')
 disp('Script para simular o algoritmo Gradiente')
 disp(' ')
-disp('Caso: Planta ............. n = 1')
+disp('Caso: Planta ............. n = 3')
 disp('      Grau relativo ..... n* = 1')
-disp('      Parâmetros ........ np = 2')
+disp('      Parâmetros ........ np = 6')
 disp(' ')
 disp('Algoritmo: Gradiente')
 disp(' ')
 disp('-------------------------------')
 
-global filter_param dc a w gamma thetas am;
+global filter_param dc a w gamma thetas am km;
 
 am = 1;
 
-plant_param = [1 2];
-filter_param = [1]';
+plant_param = -5 + 10 * rand(6,1);
+filter_param = [3 3 1]';
 
 dc = 1;
-a  = 5;
+a  = 1;
 w  = 1;
+km = 2;
 
-gamma = 10*eye(2);
+gamma = 2*eye(6);
 
-uf0 = [dc]';
-yf0 = [0]';
-theta0 = zeros(2,1);
+uf0 = [0 0 0]';
+yf0 = [0 0 0]';
+ym0 = 0;
+theta0 = zeros(6,1);
 
 %-----------------------
-thetas = [plant_param(1)' (filter_param-plant_param(2))']'; 
+thetas = [plant_param(1:3)' (filter_param-plant_param(4:6))']'; 
 
 %-----------------------
 clf;
 tf = 25;
 
-init = [theta0' uf0' yf0']';
+init = [ym0 theta0' uf0' yf0']';
 
 options = odeset('OutputFcn','odeplot');
-[T,X] = ode23s('gradiente01',tf,init,options);
+[T,X] = ode23s('gradiente03',tf,init,options);
 
-theta = X(:,1:2)';
-uf = X(:,3)';
-yf = X(:,4)';
+ym     = X(:,1);
+theta = X(:,2:7)';
+uf = X(:,8:10)';
+yf = X(:,11:13)';
 
 phi = [uf' yf']';
 y = thetas.'*phi;
 
-yhat = dot(theta, phi);
+yhat = theta.'*phi;
 
 epsilon = yhat - y;
-r = dc + a*sin(w.*T);
+r = dc + a*sin(w.*T) + a*sin(2*w.*T) + a*sin(3*w.*T) + a*sin(4*w.*T);
 modtt = sqrt(sum(theta'.^2,2))';
-
 
 figure(1)
 clf
-thetas = thetas.* ones(2,length(T));
-plot(T,theta,T,thetas);grid;shg
-legend('\theta','\theta_s','Location','SouthEast')
-print -depsc2 fig02
+subplot(211)
+plot(T,r,T,ym,T,y);grid;shg
+legend('r','y_m','y','Location','SouthEast')
+print -depsc2 fig01a
 
 figure(2)
 clf
-plot(T,modtt);grid;shg
-legend('|\theta|','Location','SouthEast')
-print -depsc2 fig03
+subplot(211)
+plot(T,theta);grid;shg
+legend('\theta','Location','SouthEast')
+print -depsc2 fig01b
 
 figure(3)
 clf
-plot(T,epsilon);grid;shg
-legend('\epsilon','Location','SouthEast')
-print -depsc2 fig04
+subplot(211)
+plot(T,modtt);grid;shg
+legend('|\theta|','Location','SouthEast')
+print -depsc2 fig01d
 
 %---------------------------------------------------------------------
 
